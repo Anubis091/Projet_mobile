fa07b041c616448352ffe4d8480318c74ce11a8e
matthieu <matthieu.91250@gmail.com>
2019-02-20 14:36:20 +0100

    code api

--- app/src/main/java/com/vogella/android/projet_mobile/MainActivity.java
+++ app/src/main/java/com/vogella/android/projet_mobile/MainActivity.java
@@ -3,8 +3,14 @@ package com.vogella.android.projet_mobile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.vogella.android.projet_mobile.controller.MainController;
+
 public class MainActivity extends AppCompatActivity {
 
+    public static void main(String[] args) {
+        MainController controller = new MainController();
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/vogella/android/projet_mobile/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.vogella.android.projet_mobile.controller;
+
+import com.vogella.android.projet_mobile.MainActivity;
+
+public class MainController {
+    private MainActivity activity;
+
+    public MainController(MainActivity mainActivity) {
+        this.activity = mainActivity;
+    }
+
+
+}
